e0879edcf5028cf9fcd9cfcc7b87e6c35a3abec1
alisabeata <alisa314159@gmail.com>
2018-12-17 19:39:07 +0200

    svg start

---
+++ 06_svg.js
@@ -0,0 +1,63 @@
+// svg
+
+
+// xml основа svg
+
+// inkscape опенсорс редактор
+// https://inkscape.org
+
+// онлайн редактор
+// https://vectr.com
+
+
+
+// - библиотеки для работы с svg
+
+// - greensock          https://greensock.com/
+// - highcharts         https://www.highcharts.com/
+// - snap               http://snapsvg.io/
+// - vivus              https://maxwellito.github.io/vivus/
+// - lazy line painter  http://lazylinepainter.info/
+// - svg js             https://svgjs.com/
+// - bonsai             https://bonsaijs.org/
+// - velocity           http://velocityjs.org/
+// - raphael            http://dmitrybaranovskiy.github.io/raphael/
+// - walkway            https://github.com/ConnorAtherton/walkway
+
+
+// animation: greensock
+// combine: bonsai, snap, svg js, velocity
+// lines: vivus, lazy line painter, walkway
+// graphs: highcharts
+// поддержка 7-8ie: raphael
+
+
+// d3 https://d3js.org/ отдельный фреймворк для визуализации данных
+
+
+
+// - базовые фигуры
+// rectangle
+// circle
+// ellipse
+// triangle
+// polyline
+// polygon
+// text
+
+// - attr
+// fill — заливка
+// stroke — цвет линии
+// stroke-width — толщина линии
+// stroke-linecap — сглаживание линии
+// stroke-linejoin — сглаживание угла
+// viewBox — размер холста
+
+// - elements
+// <g id="someId"> — группировка
+// <use xlink:href="someId"> — повторное исп элемента
+// <defs> — графика внутри defs не отображается, но на неё можно ссылаться через <use>
+// <symbols> — для группировки элементов, графика внутри не отображается пока не будет вызвана с помощью <use>, элемент исп свою систему координат, на его основе собирают svg спрайты
+// <path> — в аттр d полностью описывается путь построения https://codepen.io/anthonydugois/pen/mewdyZ (SVG Path Builder)
+
+// - viewBox
