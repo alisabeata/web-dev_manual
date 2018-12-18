079e1fb23d654c9397bf464c26dc44b9d6d915b1
alisabeata <alisa314159@gmail.com>
2018-12-18 20:07:18 +0200

    svg

--- 06_svg.js
+++ 06_svg.js
@@ -60,4 +60,65 @@
 // <symbols> — для группировки элементов, графика внутри не отображается пока не будет вызвана с помощью <use>, элемент исп свою систему координат, на его основе собирают svg спрайты
 // <path> — в аттр d полностью описывается путь построения https://codepen.io/anthonydugois/pen/mewdyZ (SVG Path Builder)
 
+
 // - viewBox
+
+// выравнивание по центру
+<svg width="200" height="200" viewBox="50 50 100 100">
+  <circle cx="100" cy="100" r="50" style="fill:green;" />
+</svg>
+  
+  
+// - preserveAspectRatio
+// пример: https://codepen.io/Krabaton/pen/RVPQje
+  
+preserveAspectRatio="xMinYMin meet"
+preserveAspectRatio="none" // сброс пропорций, полностью вписывает графику в контейнер
+
+// meet — вписывает по контуру viewBox
+// slice — срезает
+// xMinYMin — левый верхний
+// xMidYMid — середина
+// xMaxYMax — правый нижний
+
+
+
+// - gradients
+
+// градиент для текста
+<linearGradient id="gradient" x1="0" y1="0" x2="100%" y2="100%">
+  <stop offset="0%" stop-color="#00f" />
+  <stop offset="50%" stop-color="#0f0" />
+  <stop offset="100%" stop-color="#f00" />
+</linearGradient>
+  
+<text x="500" y="300" class="text" fill="url(#gradient)">Text</text> 
+
+
+// - filters
+
+// размытие по гауссу, stdDeviation степень размытия
+<filter id="filterBlur">
+  <feGaussianBlur stdDeviation="3"></feGaussianBlur>
+</filter>
+  
+<image x="" y="" width="..." height="..." xlink:href="" style="filter:url(#filterBlur)">
+  
+// - css animation
+// пример: https://codepen.io/Krabaton/pen/zoLzzP
+  
+  
+// - stroke-dasharray / stroke-dashoffset
+// рисование/заполнение линий (штриховка с анимацией)
+// пример: https://codepen.io/Krabaton/pen/VpZyRb
+<circle r="45" cx="50%" cy="50%"  transform="rotate(-90 55 55)" class="circle__second circle-15"/>
+  
+.circles:hover .circle-15 {
+   stroke-dasharray: 42.39 282.6;
+}
+// 282.6    длина окружности C = πd
+// 42.39    282.6 * 0.15 (15%)
+
+stroke-dasharray: [нарисовать] [пропустить];
+
+
