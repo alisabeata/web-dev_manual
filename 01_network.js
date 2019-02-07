0bcbc73cea56749b37d6f192e7ee90c1cf25ef71
alisabeata <alisa314159@gmail.com>
2019-02-07 12:11:18 +0200

    network

--- 01_network.js
+++ 01_network.js
@@ -1,7 +1,7 @@
 // network
 
 // restAPI — модель описания данных
+// стандарт описания запросов (https://habr.com/ru/post/351890/)
 // (Representational State Transfer) — передача состояния представления
 // HTTP-запрос, обычно get или post называется rest-запросом
 // большинство RESTful-реализаций используют стандарты, такие как HTTP, URL, JSON и XML
-
