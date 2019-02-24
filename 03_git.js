92efb4e03dfe0e2d8072cd39e189cc8a1110f843
alisabeata <alisa314159@gmail.com>
2019-02-24 20:06:59 +0200

    git

--- 03_git.js
+++ 03_git.js
@@ -18,6 +18,8 @@ git push -u origin master
 // branches
 git checkout branchname    // переключение веток
 git checkout -b branchname // создать ветку и переключиться на неё
+
+git merge otherbranchname  // смерджить ветки
                        
 // ssh
 ssh-keygen
@@ -32,9 +34,16 @@ git clone https:...git filename
 
 // log
 git log
+git log --graph
 
 // remove from index
 git rm --catched dir/filename.html
 
 // reset
 git reset --hard [commit number]
+
+
+// git flow
+// подход к организации коммитов
+// https://www.atlassian.com/git/tutorials/comparing-workflows
+// https://danielkummer.github.io/git-flow-cheatsheet/index.ru_RU.html
