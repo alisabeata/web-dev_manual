e727410307f178591534e8022788e22cf5460b7f
alisabeata <alisa314159@gmail.com>
2019-07-09 13:13:04 +0300

    network

--- 01_network.js
+++ 01_network.js
@@ -33,3 +33,14 @@ webSocket.onclose = function (event) {
 
 // запуск локального хоста на мобильном
 // https://medium.com/@wiekatz/testing-web-applications-running-on-localhost-with-an-iphone-7db6258b8f2
+
+// On the iPhone, go to Settings → Safari → Advanced and activate the switch “web inspector”
+// Connect the phone to your MacBook using a lightning-to-USB cable
+// On the MacBook, open up Safari and go to Develop → {{NAME_OF_YOUR_IPHONE}} and select “Connect via Network”
+// Open up a terminal and run the command “ifconfig” to see the list of current network interfaces and take note of the IP address of the last entry in the list
+ifconfig
+// >> inet 169.254.145.56   // example
+
+// On your phone, in Safari, use the IP address to access your locally running website. For example, if you use create-react-app, instead of going to http://localhost:3000, with the IP I got here, the address to use is http://169.254.145.56:3000 (don’t forget the http:// prefix)
+
+// You can now inspect the website displayed on your iPhone with your MacBook’s Safari by selecting it in the Develop menu
